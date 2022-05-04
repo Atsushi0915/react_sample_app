352f7f366ca913c72c8769e4e62cf1d01d39da29
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-04 16:40:14 +0900

    3-2 ステートの更新

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React, {Component} from 'react'
-import './App.css';
-import Rect from './Rect'
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
-  
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      msg: 'Hello',
+      count:0,
+    }
+    let timer = setInterval(()=>{
+      this.setState({
+        count: this.state.count + 1,
+        msg: "[ count: " + this.state.count + " ]"
+      })
+    }, 1000)
+  }
+
   render(){
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">draw rectangle.</p>
-        <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-        <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-        <Rect x="400" y="400" w="200" h="200" c="#6669" r="100"/>
+        <p className="subtitle">Count number.</p>
+        <p className="alert alert-warning">{this.state.msg}</p>
+        <p className="alert alert-dark">{this.props.msg}</p>
       </div>
     </div>
   }
 }
 
-export default App;
+export default App
