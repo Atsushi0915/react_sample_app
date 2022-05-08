cef223098b5e16361f66231f877ab969f1c6ea90
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-09 06:27:59 +0900

    4-3 更新時に処理を実行する

--- src/App.js
+++ src/App.js
@@ -1,55 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import './App.css'
 
 function AlertMessage(props){
-  const data = ["Hello!", "Welcome...", "Good-bye?"]
-
-  const actionAlert = () => {
-    const re = data[Math.floor(Math.random() * data.length)]
-    props.setAlert('message: "' + re + '".')
-  }
-
   return <div className="alert alert-primary h5 text-primary">
-    <h5>{props.alert}</h5>
-    <button onClick={actionAlert} className="btn btn-primary">
-      Click me!
-    </button>
-  </div>
-}
-
-function CardMessage(props){
-  const [count, setCount] = useState(0)
-
-  const actionCard = () => {
-    setCount(count + 1)
-    props.setCard("card counter: " + count + " count.")
-  }
-
-  return <div className="card p-3 border-dark text-center">
-    <h5>{props.card}</h5>
-    <button onClick={actionCard} className="btn btn-secondary">
-      Click me!
-    </button>
+    <h5>{props.msg}</h5>
   </div>
 }
 
 function App() {
-  const [alert, setAlert] = useState("This is alert message!")
-  const [card, setCard] = useState("This is card message!")
+  const [val, setVal] = useState(0)
+  const [msg, setMsg] = useState('set a number...')
 
-  return (
+  const doChange = (event) => {
+    setVal(event.target.value)
+  }
+
+  useEffect(() => {
+    let total = 0
+    for (let i = 0; i <= val; i++) {
+      total += i
+    }
+    setMsg("total: " + total + ".")
+  })
+
+  return(
     <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white desplay-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage alert={alert} setAlert={setAlert} />
-        <CardMessage card={card} setCard={setCard} />
-        <hr />
-        <div className="text-right">
-          <p>{alert}</p>
-          <p>{card}</p>
+        <AlertMessage msg={msg} />
+        <div className="form-group">
+          <label>Input:</label>
+          <input type="number" className="form-control" onChange={doChange} />
         </div>
       </div>
+
     </div>
   )
 }
