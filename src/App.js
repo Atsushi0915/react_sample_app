27874eea94cc3779c4a33c11a66235e50e55fc84
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-08 16:47:16 +0900

    属性で値を渡す(2)

--- src/App.js
+++ src/App.js
@@ -8,23 +8,34 @@ function AlertMessage(props){
 }
 
 function CardMessage(props){
-  return <div className="card p-3 h5 border-primary text-container">
+  return <div className="card p-3 h5 border-primary text-center">
     {props.message}
   </div>
 }
 
-function App(){
-  const [msg] = useState("This is sample message!!!!")
+function App() {
+  const [msg, setMsg] = useState("This is sample message!")
+
+  const doAction = () => {
+    let res = window.prompt('type your name:')
+    setMsg("Hello, " + res + "!!")
+  }
 
   return (
     <div>
-      <h1 className="bg-primary text-white desplay-4">React</h1>
+      <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4 className="my-3">Hooks sample</h4>
+        <h4 className="my-3">Hooks sampl</h4>
         <AlertMessage message={msg} />
         <CardMessage message={msg} />
+        <div className="text-center">
+          <button onClick={doAction} className="btn btn-primary">
+            Click me!
+          </button>
+        </div>
       </div>
     </div>
+    
   )
 }
 
