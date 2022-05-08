f09f47c3963de6d9165c3dc530dbf7b6a17807d1
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-08 14:14:08 +0900

    4-2 コンポーネントにコンポーネントを組み込む

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React, { useState } from 'react'
 import './App.css'
 
-function App(){
-  const [count, setCount] = useState(0)
-  const [flag, setFlag] = useState(false)
-  
-  const clickFunc = () => {
-    setCount(count + 1)
-  }
+const  AlertMessage = () => {
+  return <div className="alert alert-primary h5 text-primary">
+    This is Alert message!
+  </div>
+}
 
-  const changeFlag = (e) => {
-    setFlag(e.target.checked)
-  }
+const CardMessage = () => {
+  return <div className="card p-3 h5 border-primary text-center">
+    This is Card message!
+  </div>
+}
 
+const App = () => {
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        {flag ?
-          <div className="aler alert-primary text-center">
-            <p className="h5 mb-3 pt-3">click: {count} times!</p>
-            <div>
-              <button className="btn btn-primary mb-3" onClick={clickFunc}>
-                Click me
-              </button>
-            </div>
-          </div>
-        :
-          <div className="card p-3 border-primary text-center">
-            <p className="h5 mb-3 text-left text-primary">
-              click: {count} times!
-            </p>
-            <div>
-              <button className="btn btn-primary" onClick={clickFunc}>
-                Click me
-              </button>
-            </div>
-          </div>
-        }
-        <div className="form-group h6 text-center pt-3">
-          <input type="checkbox" className="form-check-input" id="check1" onChange={changeFlag} />
-          <label className="form-check-label" htmlFor="check1">
-            Change form style.
-          </label>
-        </div>
+        <AlertMessage />
+        <CardMessage />
       </div>
     </div>
   )
 }
 
 export default App
-
-
