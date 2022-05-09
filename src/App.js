351f06ce0a5c3c85a1a6abc442a9fcba90357a51
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-10 07:24:00 +0900

    4-4 より複雑な操作を行うフック

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-function useCounter() {
-  const [num, setnum] = useState(0)
 
-  const count = () => {
-    setnum(num + 1)
+const useTax = (t1, t2) => {
+  const [price, setPrice] = useState(1000)
+  const [tx1] = useState(t1)
+  const [tx2] = useState(t2)
+
+  const tax = () => {
+    return Math.floor(price * (1.0 + tx1 / 100))
+  }
+  const reduced = () => {
+    return Math.floor(price * (1.0 + tx2 / 1000))
   }
 
-  return [num, count]
+  return [tax, reduced, setPrice]
 }
 
-function AlertMessage(props){
-  const [counter, plus] = useCounter()
-  return <div className="alert alert-primary h5 text-center">
-    <h4>count: {counter} .</h4>
-    <button onClick={plus} className="btn btn-primary">
-      count
-    </button>
+function AlertMessage(props) {
+  const [price, tax, reduced, setPrice] = useTax(10,8)
+
+  const DoChange = (e) => {
+    let p = e.target.value
+    setPrice(p)
+  }
+
+  return <div className="alert alert-primary h5">
+    <p className="h5">通常税率: {tax()} 円. </p>
+    <p className="h5">軽減税率: {reduced()} 円. </p>
+    <div className="form-group">
+      <label className="form-group-label">Price: </label>
+      <input type="number" className="form-control" onChange={DoChange} value={price} />
+    </div>
   </div>
 }
 
 function App() {
-  return (
+  return(
     <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white display-4" >React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
         <AlertMessage />
